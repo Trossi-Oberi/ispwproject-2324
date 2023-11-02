b03cc8a5e200265f6edea5105412afe1130e4b03
DeveloperMatt02 <matteotrossi002@gmail.com>
2023-11-02 10:44:28 +0100

    Updated main.java, added README.md file. Created deliverables directory with Use Case diagram.

--- src/main/java/it/uniroma2/ispw/Main.java
+++ src/main/java/it/uniroma2/ispw/Main.java
@@ -7,7 +7,6 @@ public class Main {
         // Press Alt+Invio with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-        System.out.println("STAMPA SNYPPO");
 
     }
 }
