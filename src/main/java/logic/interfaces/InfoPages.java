9d8f3d53150609aec65f98b25330c29844c9cecb
snyppololo <niky2527@gmail.com>
2024-01-30 11:45:50 +0100

    comment on infopages interface

--- src/main/java/logic/interfaces/InfoPages.java
+++ src/main/java/logic/interfaces/InfoPages.java
@@ -5,5 +5,6 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public interface InfoPages {
+    //implementata da GCHelpFAQs e GCContactUs
     public void openLink(ActionEvent event) throws URISyntaxException, IOException;
 }
