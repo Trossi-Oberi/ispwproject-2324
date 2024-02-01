0db13bff6e81d3dfcbd2759da66443cb6d8e75c3
DeveloperMatt02 <matteotrossi002@gmail.com>
2024-02-01 16:51:16 +0100

    updated singletonDB session manager - information hiding usr and password

--- src/main/java/logic/utils/SingletonDBSession.java
+++ src/main/java/logic/utils/SingletonDBSession.java
@@ -1,23 +1,55 @@
 package logic.utils;
 
+import java.io.InputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
 public class SingletonDBSession {
     private static SingletonDBSession istance = null;
-    private Logger logger;
-    private String url = "jdbc:mysql://localhost/nightplan";
-    private String username = "root";
-    private String password = "Trossioberi2024!";
-
+    private final String username;
+    private final String password;
+    private final String url;
+    private InputStream inputStream;
+    private static final Logger logger = Logger.getLogger("NightPlan");
     protected Connection connection = null;
 
 
-    protected SingletonDBSession() {
-        this.logger = Logger.getLogger("NightPlan");
+    public String[] getPropValues() throws IOException {
+        String[] result = new String[2];
+        try {
+            Properties prop = new Properties();
+            String propFileName = "config.properties";
+            inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+            prop.load(inputStream);
+            result[0] = prop.getProperty("username");
+            result[1] = prop.getProperty("password");
+        } catch (NullPointerException e) {
+            logger.log(Level.SEVERE, "Properties file not found!", e);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Properties cannot be loaded!", e);
+            throw new IOException();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return result;
+    }
+
+    private SingletonDBSession() throws RuntimeException {
+        this.url = "jdbc:mysql://localhost/nightplan";
+        try {
+            this.username= getPropValues()[0];
+            this.password = getPropValues()[1];
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Connection getConnection() {
@@ -33,7 +65,11 @@ public class SingletonDBSession {
     public static synchronized SingletonDBSession getInstance(){
         //singleton method
         if(SingletonDBSession.istance == null) {
-            SingletonDBSession.istance = new SingletonDBSession();
+            try {
+                SingletonDBSession.istance = new SingletonDBSession();
+            } catch (RuntimeException e) {
+                logger.log(Level.SEVERE, "Runtime exception in DB session manager");
+            }
         }
         return SingletonDBSession.istance;
     }
