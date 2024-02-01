091a21b27289d92fe4a3134a8ec862f1fa88f0a3
DeveloperMatt02 <matteotrossi002@gmail.com>
2024-02-01 20:00:26 +0100

    added event click listener on home user list view - to be finished

--- src/main/java/logic/graphiccontrollers/GCHomeUser.java
+++ src/main/java/logic/graphiccontrollers/GCHomeUser.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
 import logic.beans.BEvent;
+import logic.utils.Alerts;
 import logic.utils.LoggedUser;
 
 import java.util.ArrayList;
@@ -24,15 +25,37 @@ public class GCHomeUser extends GCHomeGeneral {
     public void initialize(){
         //inserire retrive eventi della tua città all'avvio della schermata
         this.eventsList = cfacade.retrieveEvents(LoggedUser.getUserType(), "HomeUser.fxml");
-        populateEventsListView();
+        populateEventsListViews();
+        setupEventClickListener();
     }
-
-    private void populateEventsListView(){
+    private void populateEventsListViews() {
         for (int i = 0; i < this.eventsList.size(); i++) {
             this.eventsListView.getItems().add(this.eventsList.get(i).getEventName());
+            this.musicListView.getItems().add(this.eventsList.get(i).getEventMusicGenre());
+            this.groupsListView.getItems().add("No group");
         }
     }
 
+    private void setupEventClickListener(){
+        eventsListView.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2) {
+                // Verifica se è stato effettuato un doppio clic
+                onItemDoubleClick(eventsListView.getSelectionModel().getSelectedItem());
+            }
+        });
+        groupsListView.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2) {
+                // Verifica se è stato effettuato un doppio clic
+                goToSettings(event);
+            }
+        });
+    }
+
+    private void onItemDoubleClick(String selectedItem) {
+        // Esegui la tua funzione con l'elemento selezionato
+        //System.out.println("Evento cliccato: " + selectedItem);
+        alert.displayAlertPopup(Alerts.CONFIRMATION, "Evento selezionato: " + selectedItem);
+    }
 }
 
 
