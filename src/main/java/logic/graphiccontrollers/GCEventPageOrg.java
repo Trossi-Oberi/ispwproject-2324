17bed4765bfec4f115026ddd921975b375b0e6e7
DeveloperMatt02 <matteotrossi002@gmail.com>
2024-02-05 19:58:01 +0100

    fixed eventpageuser goback button added maven dependency for javafx-web

--- src/main/java/logic/graphiccontrollers/GCEventPageOrg.java
+++ src/main/java/logic/graphiccontrollers/GCEventPageOrg.java
@@ -26,7 +26,7 @@ public class GCEventPageOrg extends GCEventPageGeneral {
 
     @FXML
     void goBack(MouseEvent event) {
-
+        goToYourEvents(event);
     }
 
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -14,6 +14,7 @@ module trossi.oberi {
     requires transitive javafx.graphics;
 
     requires org.json;
+    requires javafx.web;
 
     exports logic.view;
     exports logic.graphiccontrollers;
