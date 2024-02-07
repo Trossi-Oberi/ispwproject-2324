59356e03e9acbc697e40990c38016d246dc8da21
DeveloperMatt02 <matteotrossi002@gmail.com>
2024-02-07 12:02:17 +0100

    fixed map in event page user.fxml

--- src/main/java/logic/graphiccontrollers/GCEventPageUser.java
+++ src/main/java/logic/graphiccontrollers/GCEventPageUser.java
@@ -1,20 +1,35 @@
 package logic.graphiccontrollers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Text;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import logic.exceptions.DuplicateEventParticipation;
+import logic.interfaces.InfoPages;
 import logic.utils.Alerts;
-import logic.utils.GoogleMapsAPI;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
+import java.net.URLEncoder;
 
-public class GCEventPageUser extends GCEventPageGeneral {
+public class GCEventPageUser extends GCEventPageGeneral implements InfoPages {
 
     @FXML
     private Button participateEventBtn;
+
+    @FXML
+    private Button viewMap;
 
     @FXML
     void goBack(MouseEvent event) {
@@ -36,12 +51,15 @@ public class GCEventPageUser extends GCEventPageGeneral {
             goBack(event);
         }
     }
-    @FXML
-    void openMap(MouseEvent event) {
 
+    @Override
+    public void openLink(ActionEvent event) throws URISyntaxException, IOException {
+        String link = generateMapLink(eventBean.getEventAddress());
+        Desktop.getDesktop().browse(new URI(link));
     }
 
-
-
-
+    private static String generateMapLink(String address){
+        String encodedAddress = URLEncoder.encode(address, StandardCharsets.UTF_8);
+        return "https://www.google.com/maps/search/?api=1&query=" + encodedAddress;
+    }
 }
