daf1f59c523cfc5298e1f32c8eae79db231b6ff7
DeveloperMatt02 <matteotrossi002@gmail.com>
2024-02-08 12:07:10 +0100

    updated module-info.java to match new Google Auth dependencies in pom.xml (maven dependencies)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,10 +11,18 @@ module trossi.oberi {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires javafx.web;
     requires transitive javafx.graphics;
 
     requires org.json;
-    requires javafx.web;
+
+    requires google.api.client;
+    requires com.google.api.client;
+    requires com.google.api.client.auth;
+    requires com.google.api.client.extensions.java6.auth;
+    requires com.google.api.client.extensions.jetty.auth;
+    requires com.google.api.client.json.gson;
+    requires com.google.api.client.json.jackson2;
 
     exports logic.view;
     exports logic.graphiccontrollers;
