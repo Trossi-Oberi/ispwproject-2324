1945376350cd1971c65166cc77070e5f81eee4d1
DeveloperMatt02 <matteotrossi002@gmail.com>
2024-02-18 15:11:16 +0100

    corrected serialization exception handler changed ClientListener constructor signature

--- src/main/java/logic/controllers/ClientListener.java
+++ src/main/java/logic/controllers/ClientListener.java
@@ -4,7 +4,6 @@ import logic.model.Notification;
 import logic.utils.LoggedUser;
 import logic.utils.SecureObjectInputStream;
 import logic.utils.UserTypes;
-import logic.view.EssentialGUI;
 import logic.view.NotificationView;
 
 import java.io.IOException;
@@ -14,21 +13,18 @@ import static logic.view.EssentialGUI.logger;
 
 public class ClientListener extends Thread implements Runnable{
     //il client listener si occupa di gestire la comunicazione in input da server a client
-
-    private int clientID;
-    private CNotification notificationCtrl;
     private Semaphore semaphore;
     private SecureObjectInputStream in;
     private boolean listenerRunning = true;
     private NotificationView notificationView;
 
-    public ClientListener(NotificationView notiView, int id, Semaphore semaphore, CNotification notiController, SecureObjectInputStream in){
+    public ClientListener(NotificationView notiView, Semaphore semaphore, SecureObjectInputStream in){
         this.notificationView = notiView;
-        this.clientID = id;
         this.semaphore = semaphore;
-        this.notificationCtrl = notiController;
         this.in = in;
     }
+
+
 
     @Override
     public void run() {
@@ -36,10 +32,10 @@ public class ClientListener extends Thread implements Runnable{
             semaphore.acquire();
 
             //una volta aperta socket e canali di comunicazione aspetto l'arrivo di un qualunque messaggio dal server
-            while(listenerRunning){
+            while (listenerRunning) {
                 Notification incomingMsg = (Notification) in.readObject();
-                if(incomingMsg != null){
-                    switch (incomingMsg.getNotificationType()){
+                if (incomingMsg != null) {
+                    switch (incomingMsg.getNotificationType()) {
                         case UserRegistration:
                             System.out.println("New client " + incomingMsg.getClientID() + " successfully registered.");
                             //chiudo i canali di comunicazione del client con il server
@@ -54,10 +50,10 @@ public class ClientListener extends Thread implements Runnable{
                             break;
 
                         case EventAdded:
-                            if(LoggedUser.getUserType() == UserTypes.ORGANIZER){
+                            if (LoggedUser.getUserType() == UserTypes.ORGANIZER) {
                                 //rilascia semaforo solo per organizer per non bloccare l'applicazione
                                 semaphore.release(2);
-                            } else{
+                            } else {
 
                                 //chiama la giusta funzione in base alla GUI per lo user
                                 notificationView.showNotification(incomingMsg.getNotificationType());
@@ -71,9 +67,9 @@ public class ClientListener extends Thread implements Runnable{
                             break;
 
                         case UserEventParticipation:
-                            if (LoggedUser.getUserType() == UserTypes.USER){
+                            if (LoggedUser.getUserType() == UserTypes.USER) {
                                 semaphore.release(2);
-                            }else{
+                            } else {
                                 //chiama la giusta funzione in base alla GUI per notificare l'organizer
                                 notificationView.showNotification(incomingMsg.getNotificationType());
                             }
@@ -85,7 +81,7 @@ public class ClientListener extends Thread implements Runnable{
                             break;
 
                         case GroupJoin:
-                            System.out.println("SERVER: group with id "+incomingMsg.getEventID()+" joined successfully");
+                            System.out.println("SERVER: group with id " + incomingMsg.getEventID() + " joined successfully");
                             semaphore.release(2);
                             break;
 
@@ -103,9 +99,14 @@ public class ClientListener extends Thread implements Runnable{
                     }
                 }
             }
-        } catch (InterruptedException | IOException | ClassNotFoundException e) {
+
+        } catch (ClassNotFoundException | IOException e) {
+            //gestione eccezioni di deserializzazione (readObject)
+            logger.severe("Cannot serialize object");
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } catch (IllegalArgumentException e){
+            //gestione rilascio semafori
             logger.severe(e.getMessage());
         }
     }
