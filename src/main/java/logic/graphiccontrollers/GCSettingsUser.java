b6820d7ba72285ea51e1f11cfb9affbebc3146f1
DeveloperMatt02 <matteotrossi002@gmail.com>
2024-02-20 01:42:59 +0100

    removed YourGroups button

--- src/main/java/logic/graphiccontrollers/GCSettingsUser.java
+++ src/main/java/logic/graphiccontrollers/GCSettingsUser.java
@@ -3,22 +3,14 @@ package logic.graphiccontrollers;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Button;
-import logic.utils.Alerts;
 
 public class GCSettingsUser extends GCSettingsGeneral {
     @FXML
     private Button changeCityBtn;
 
     @FXML
-    private Button yourGroupsBtn;
-
-    @FXML
     void goToChangeCity(MouseEvent event) {
         changeGUI(event, "EditCity.fxml");
     }
 
-    @FXML
-    void goToYourGroups(MouseEvent event) {
-        alert.displayAlertPopup(Alerts.ERROR, "Not implemented yet :S");
-    }
 }
