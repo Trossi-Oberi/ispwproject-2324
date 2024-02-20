b47de7b5d2e3ac746d698c378b37562bb7c1ba72
snyppololo <niky2527@gmail.com>
2024-02-20 14:20:43 +0100

    Smell, implemented private constructor

--- src/main/java/logic/controllers/CServerInteraction.java
+++ src/main/java/logic/controllers/CServerInteraction.java
@@ -3,6 +3,8 @@ package logic.controllers;
 import java.util.concurrent.Semaphore;
 
 public class CServerInteraction {
+
+    private CServerInteraction(){};
     protected static Semaphore semaphore;
     protected static ClientListener listener;
     protected static Thread listenerThread;
