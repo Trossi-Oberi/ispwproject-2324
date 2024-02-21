d6017861114edca82b07cac5f364d0b8b45e0c35
DeveloperMatt02 <matteotrossi002@gmail.com>
2024-02-21 18:52:29 +0100

    fixed smells

--- src/main/java/logic/controllers/CFacade.java
+++ src/main/java/logic/controllers/CFacade.java
@@ -91,7 +91,8 @@ public class CFacade {
         }
         boolean res = manageEventController.removeEventParticipation(eventBean);
         if (res) {
-            result = leaveGroupAfterRemoveEventPart(eventBean);
+            Integer groupID = getGroupByEventID(eventBean.getEventID()).getGroupID();
+            result = leaveGroupAfterRemoveEventPart(eventBean, groupID);
             if (result) {
                 if (notificationController == null) {
                     notificationController = new CNotification(this);
@@ -104,8 +105,7 @@ public class CFacade {
         return result;
     }
 
-    private boolean leaveGroupAfterRemoveEventPart(BEvent eventBean){
-        Integer groupID = getGroupByEventID(eventBean.getEventID()).getGroupID();
+    private boolean leaveGroupAfterRemoveEventPart(BEvent eventBean, Integer groupID){
         //se il gruppo non esiste salto il leaveGroup
         if (groupID == null || !checkUserInGroup(groupID)) {
             //gruppo non esistente o utente non nel gruppo
