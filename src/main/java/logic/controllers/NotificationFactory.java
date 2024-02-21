ac12364a06d77d5e04e0c3611f61e3510c07524b
DeveloperMatt02 <matteotrossi002@gmail.com>
2024-02-21 01:15:31 +0100

    fixed small smell ;)(

--- src/main/java/logic/controllers/NotificationFactory.java
+++ src/main/java/logic/controllers/NotificationFactory.java
@@ -8,24 +8,6 @@ import logic.utils.UserTypes;
 public class NotificationFactory {
     //questa factory crea i 2 tipi di notifiche: ServerClient e Local
 
-/*    public Notification createNotification(SituationType sitType, NotificationTypes notiType, Integer clientOrNotifiedID, Integer notifierID, Integer eventID, Integer notificationID, String city, String newCity, UserTypes usrType) {
-        Notification noti;
-        if (sitType == SituationType.SERVER_CLIENT) {
-            noti = new ServerNotification();
-        } else {
-            noti = new LocalNotification();
-        }
-        noti.setNotificationType(notiType);
-        noti.setClientID(clientOrNotifiedID);
-        noti.setNotifierID(notifierID);
-        noti.setEventID(eventID);
-        noti.setNotificationID(notificationID);
-        noti.setCity(city);
-        noti.setNewCity(newCity);
-        noti.setUserType(usrType);
-        return noti;
-    }*/
-
     public Notification createNotification(SituationType sitType, NotificationTypes notiType, Integer clientOrNotifiedID, NotificationProperties notiProps, Integer eventID, CityData cityData, UserTypes usrType) {
         Notification noti;
         if (sitType == SituationType.SERVER_CLIENT) {
